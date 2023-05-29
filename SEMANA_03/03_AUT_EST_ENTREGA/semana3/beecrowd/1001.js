e5370128aae3ea2546001256d3e3b5a9048ea103
Paula-zp <paula.piva@sou.inteli.edu.br>
2023-05-29 08:48:24 -0300

    gitpage

--- SEMANA_03/03_AUT_EST_ENTREGA/semana3/beecrowd/1001.js
+++ SEMANA_03/03_AUT_EST_ENTREGA/semana3/beecrowd/1001.js
@@ -1,10 +1,23 @@
 var input = require('fs').readFileSync('stdin', 'utf8');
-var lines = input.split('\n');
-console.log(typeof lines);
+var line = input.trim().split('\n')[0];
+
 //o código a seguir é que irá para o Bee Crowd
-var A = parseInt(lines.shift());
-var B = parseInt(lines.shift());
 
-var X = A + B;
+var vogais = ["a", "e", "i", "o", "u"]
+palavra = line.split("")
 
-console.log('X = ' + X);
+palavra = palavra.filter(letra => vogais.includes(letra))
+
+console.log(palavra)
+
+if(palavra.join("") == palavra.slice().reverse().join("")){
+    console.log("S")
+    console.log(palavra.join(""))
+    console.log(palavra.slice().reverse().join(""))
+}
+
+else{
+    console.log("N")
+    console.log(palavra.join(""))
+    console.log(palavra.slice().reverse().join(""))
+}
